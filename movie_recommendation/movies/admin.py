ec5e7a269be211ca5f68d824bf080176310ad60e
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2024-08-26 23:58:09 +0330

    Enhance Django admin dashboard for managing movies, genres, and user profiles.

--- movie_recommendation/movies/admin.py
+++ movie_recommendation/movies/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from .models import Genre, Movie, UserProfile
+from .models import Genre, Movie, UserProfile, Watchlist
 
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(UserProfile)
 
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'release_date', 'rating')
+    search_fields = ('title', 'genre__name')
+    list_filter = ('genre', 'release_date')
+    ordering = ('-release_date',)  # Default order by release date (newest first)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+    search_fields = ('user__username', 'user__email')
+    filter_horizontal = ('favorite_genres',)  # Makes it easier to manage many-to-many relationships
+
+
+@admin.register(Watchlist)
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'movie')
+    search_fields = ('user__username', 'movie__title')
